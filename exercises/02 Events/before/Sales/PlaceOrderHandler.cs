872a16285f4a2ed2c5e08eeefd69ac45815460b3
Dennis van der Stelt <dvdstelt@gmail.com>
2019-03-06 21:47:43 +0100

    Added exercise 02

---
+++ exercises/02 Events/before/Sales/PlaceOrderHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#pragma warning disable 162
+
+namespace Sales
+{
+    public class PlaceOrderHandler :
+        IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            // This is normally where some business logic would occur
+
+            return Task.CompletedTask;
+        }
+    }
+}
